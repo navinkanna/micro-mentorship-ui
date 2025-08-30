a8e2084d37f6223e8e2816bc3e32fb88e0f55444
Navin Kanna <navinkumark1997@gmail.com>
2025-08-29 19:13:03 -0500

    Second Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login-component/login-component';
+import { HomeComponent } from './pages/home-component/home-component';
+import { SignUpComponent } from './pages/sign-up-component/sign-up-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignUpComponent }, 
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: '' }
+  ];
+  
