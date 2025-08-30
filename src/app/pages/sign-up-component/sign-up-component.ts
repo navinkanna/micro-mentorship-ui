0dbf37c2e96c095c4ae5d1219f53041cea351b1b
Navin Kanna <navinkumark1997@gmail.com>
2025-08-30 16:31:19 -0500

    Signup Initial

--- src/app/pages/sign-up-component/sign-up-component.ts
+++ src/app/pages/sign-up-component/sign-up-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-component',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up-component.scss'
 })
 export class SignUpComponent {
+constructor(private router: Router){}
 
+backToHome(){
+  // Navigate back to home
+  this.router.navigate(['/home']);
+}
 }
